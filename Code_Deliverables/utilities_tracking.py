f6b0cab9de042f35697cb4f5e4eff4a87765f052
ktan0024 <&Githubktan0024>
2020-05-21 00:28:08 +0800

    add comment to utilities_tracking.py

--- Code_Deliverables/utilities_tracking.py
+++ Code_Deliverables/utilities_tracking.py
@@ -2,7 +2,7 @@ import cv2
 
 def remove_duplicate(cur_names, cur_locs, cur_prob):
     """
-    Description : This function select the faces names with highest probability, if there is multiple persons recognized as the same identity.
+    Description : This function select only one of the duplicate faces names with highest probability, if there is multiple persons recognized as the same identity.
     Author : Tan Kai Yi
     Last modified : 20/05/2020
     param :
@@ -24,18 +24,22 @@ def remove_duplicate(cur_names, cur_locs, cur_prob):
         name = cur_names[i]
         prob = cur_prob[i]
         max_id = i
+        ## only compare the probability of duplicate name which is not unknown
         if name != "unknown":
             if not name in names:
                 names.append(name)
+                ## loop through the current face names, check if there is any duplication
                 for j in range(len(cur_names)):
                     name2 = cur_names[j]
                     prob2 = cur_prob[j]
+                    ## if a duplication is found, compare the probability and store the index of name with highest probability
                     if name2 == name and prob2 > prob and name2 != "unknown":
                         max_id = j
                         prob = prob2
 
                 probability.append(prob)
                 locations.append(cur_locs[max_id])
+        ## if a name is unknown, just keep it, it might use for future update in track function
         else:
             names.append(name)
             probability.append(prob)
@@ -46,7 +50,8 @@ def remove_duplicate(cur_names, cur_locs, cur_prob):
 
 def update(cur_names, pre_names, cur_locs, pre_locs, cur_prob, pre_prob, false_track):
     """
-    Description : This function is used to untrack the person who continuously fail to be recognized (false track) more than a specific times (threshold) in the recgnition process
+    Description : This function is used to untrack the person who continuously fail to be recognized (false track) more than a specific times (threshold) in the recgnition process.
+                    Meanwhile, if a person is not recognized in current frame but successfully recognized or tracked in previous frame, it will continue to track the person until the person has false track more than specific time
     Author : Tan Kai Yi
     Last modified : 20/05/2020
     param :
@@ -65,12 +70,15 @@ def update(cur_names, pre_names, cur_locs, pre_locs, cur_prob, pre_prob, false_t
             false_track: the updated dictionary of the number of false track for each person. If a person has false track more than a specific time, it will be removed in the cur_names list and his/her false track count will reset to 0 in dictionary
 
     """
+    ## set a threshold, to decide untrack a person after how many times of false track
     threshold = 2
+    ## the names array only store the name with false track less than threshold
     names = []
     locations = []
     probability = []
 
     unknowns = []
+    # the cur_names is the current recognized faces. Once a face is successfully recognized, its false track will reset to 0. The dictionary store the number of false track "continuously" of a person
     for i in range(len(cur_names)):
         name = cur_names[i]
         if name == "unknown":
@@ -78,23 +86,30 @@ def update(cur_names, pre_names, cur_locs, pre_locs, cur_prob, pre_prob, false_t
         else:
             false_track[name] = 0
 
+    ## process to count false track
     for i in range(len(pre_names)):
         name = pre_names[i]
+        ## only check names is not in cur_names. If a name is in cur_names, means it is successfully recognized. There is no need to count false track
         if name not in cur_names and name != "unknown":
+            ## if the name is first time has a false track, add it to dictionary with value 0
             if name not in false_track:
                 false_track[name] = 0
             else:
+                ## if a name have a number of false track larger than threshold, reset the count back to 0
                 if false_track[name] > threshold:
                     false_track[name] = 0
+                ## otherwise, add the name to names array, add its location to locations array, and add its probability to probability array
                 else:
                     false_track[name] += 1
                     names.append(name)
                     locations.append(pre_locs[i])
                     probability.append(pre_prob[i])
 
+    ## if names is empty, it means all previous detected faces is either recognized or have a false track more than threshold in current frame. No further update required.
     if len(names) == 0:
         return cur_names, cur_prob, cur_locs, false_track
 
+    ## process to update the person recognized as "unknown", but successfully recognized or tracked in previous frame with false track less than threshold
     for n in range(len(names)):
         face = locations[n]
         min_dif = None
@@ -103,6 +118,7 @@ def update(cur_names, pre_names, cur_locs, pre_locs, cur_prob, pre_prob, false_t
             cur_id = unknowns[i][0]
             face1 = cur_locs[cur_id]
             dif = abs(face1[0] - face[0]) + abs(face1[1] - face[1]) + abs(face1[2] - face[2]) + abs(face1[3] - face[3])
+            ## a threshold to check if there is any recognized faces in previous frame is close to the current detected but not recognized face location. If there is multiple, choose the closest one
             if dif <= 300:
                 if min_dif == None or min_dif > dif:
                     min_dif = dif
@@ -113,6 +129,7 @@ def update(cur_names, pre_names, cur_locs, pre_locs, cur_prob, pre_prob, false_t
                 unknowns[min_id] = (cur_id, min_dif, n)
 
     for i in range(len(unknowns)):
+        ## if any unknown person found a recognized face's location in previous frame which is close to him, update his/her name and probability to that face
         if unknowns[i][1] != -1:
             cur_id = unknowns[i][0]
             cur_names[cur_id] = names[unknowns[i][2]]
@@ -121,12 +138,13 @@ def update(cur_names, pre_names, cur_locs, pre_locs, cur_prob, pre_prob, false_t
     return cur_names, cur_prob, cur_locs, false_track
 
 
-def track(pre_faces, cur_locs, cur_names, probability):
+def track(pre_locs, cur_locs, cur_names, probability):
     """
-    Description : This function to track person who is successfully recognized, as the recognition process will only run once per n frames. During this period, the person need to be tracked.
+    Description : Function to track person who is successfully recognized in previous frame, as the recognition process will only run once per n frames. During this period, the person need to be tracked.
     Author : Tan Kai Yi
     Last modified : 20/05/2020
     param :
+            pre_locs: the previous detected faces locations in previous frame
             cur_names: the current detected faces names, faces cant be recognized will be named "unknown"
             cur_locs: the current detected faces locations
             probability: the current detected faces probabilities. The probability show the similarity of a person to the recognition result identity
@@ -140,19 +158,22 @@ def track(pre_faces, cur_locs, cur_names, probability):
     results = []
     results_names = []
     results_prob = []
-    for i in range(len(pre_faces)):
+    for i in range(len(pre_locs)):
         results.append((-1, (-1, -1, -1, -1)))
         results_names.append(cur_names[i])
         results_prob.append(probability[i])
 
+    ## for each face in current detected faces, find the closet face which is detected in previous frame, and update its name, new location and probability.
     for n in range(len(cur_locs)):
         face = cur_locs[n]
         min_dif = None
         min_id = -1
-        for i in range(len(pre_faces)):
-            face1 = pre_faces[i]
+        for i in range(len(pre_locs)):
+            face1 = pre_locs[i]
             dif = abs(face1[0] - face[0]) + abs(face1[1] - face[1]) + abs(face1[2] - face[2]) + abs(face1[3] - face[3])
+            ## a threshold to decide if the faces detected in previous frame is close enough to current face
             if dif <= 300:
+                ## choose the closest face's information which detected in previous frame
                 if (min_dif == None or min_dif > dif):
                     min_dif = dif
                     min_id = i
